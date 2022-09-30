4a4880d87cb2761418d9c583cffa7d9813b7eab5
linnforsman <linnforsman@protonmail.com>
2022-09-30 23:28:32 +0200

    commit

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -20,19 +20,63 @@
 	}
 </script>
 
-<form on:submit|preventDefault={login} class="flex mx-auto col-6 text-hotpink-400">
-	<div class="mb-3">
-		<label for="username" class="form-label">Username</label>
-		<input type="email" class="form-control" id="username" bind:value={username} />
-	</div>
+<div
+	class="relative py-16
+                before:absolute before:inset-0 before:w-full before:h-[50%] before:bg-gray-200"
+>
+	<div class="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40">
+		<div class="m-auto space-y-8 md:w-8/12 lg:">
+			<div class="rounded-xl border bg-opacity-50 backdrop-blur-2xl bg-white shadow-xl">
+				<div class="p-6 sm:p-16">
+					<h2 class="mb-8 text-2xl text-hotpink-400 uppercase font-thin">Login</h2>
+					<form
+						on:submit|preventDefault={login}
+						class="flex mx-auto col-6 text-hotpink-400"
+						action=""
+					>
+						<div class="space-y-2">
+							<label for="email" class="text-gray-700">Username</label>
+							<input
+								type="email"
+								class="form-control block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
+							focus:ring-2 focus:ring-sky-300 focus:outline-none
+							invalid:ring-2 invalid:ring-red-400"
+								id="username"
+								bind:value={username}
+							/>
+						</div>
 
-	<div class="mb-3">
-		<label for="password" class="form-label">Password</label>
-		<input type="password" class="form-control" id="password" bind:value={password} />
-	</div>
+						<div>
+							<div class="flex items-center justify-between">
+								<label for="pwd" class="text-gray-700">Password</label>
+								<button class="p-2 -mr-2" type="reset">
+									<span class="text-sm text-sky-500">Forgot your password ?</span>
+								</button>
+							</div>
 
-	<button type="submit" class="btn btn-primary">Submit</button>
-	<div id="error_message" class="text-danger">
-		<small>{error}</small>
+							<input
+								type="password"
+								class="form-control  block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
+							focus:ring-2 focus:ring-sky-300 focus:outline-none
+							invalid:ring-2 invalid:ring-red-400"
+								id="password"
+								bind:value={password}
+							/>
+						</div>
+
+						<button
+							type="submit"
+							class="w-full py-3 px-6 rounded-md bg-sky-600
+                                    focus:bg-sky-700 active:bg-sky-500"
+						>
+							<span class="text-white">Continue</span>
+						</button>
+						<div id="error_message" class="text-danger">
+							<small>{error}</small>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
-</form>
+</div>
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,1 +1,11 @@
-Fucking login
+<script>
+	import { store } from '../../hooks/auth';
+	import Logged from '$lib/components/Logged.svelte';
+	import Login from '$lib/components/Login.svelte';
+</script>
+
+{#if $store != null}
+	<Logged />
+{:else}
+	<Login />
+{/if}
